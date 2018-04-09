24e94569bfa18007eed81ea577c862bb037350c6
kio97 <olvera.elian@gmail.com>
2018-04-09 13:15:35 -0500

    Se agrego boton-chido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BotonChidoComponent } from './components/boton-chido/boton-chido.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotonChidoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/boton-chido/boton-chido.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-chido',
+  templateUrl: 'boton-chido.component.html',
+  styleUrls: ['boton-chido.component.css']
+})
+
+export class BotonChidoComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
